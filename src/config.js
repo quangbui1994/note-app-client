34a4bbe66cdf9bc55711872329f697ab3d287248
Quang Bui <buiphuquang2412@gmail.com>
2020-03-22 18:02:20 +0200

    set up aws

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "quang-note-app"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://vcojl4ocw8.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_kl7b9ifpe",
+      APP_CLIENT_ID: "186v9dhnhidnp3ql0t6ng6pl0s",
+      IDENTITY_POOL_ID: "us-east-1:8033a063-0585-4da9-9da0-8d5241ec0fa3"
+    }
+};
