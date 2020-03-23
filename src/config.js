bdcbfef1186c56bea1f7bd51956ccdf27db9d0a0
Quang Bui <buiphuquang2412@gmail.com>
2020-03-23 18:29:40 +0200

    Build App

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_hI9gTHgAM7cnvLdhhS8oyYpH00bi0TcclJ",
     s3: {
       REGION: "us-east-1",
-      BUCKET: "quang-note-app"
+      BUCKET: "quang-notes-app-uploads"
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://vcojl4ocw8.execute-api.us-east-1.amazonaws.com/prod"
+      URL: "https://wfjjne7z93.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_kl7b9ifpe",
-      APP_CLIENT_ID: "186v9dhnhidnp3ql0t6ng6pl0s",
-      IDENTITY_POOL_ID: "us-east-1:8033a063-0585-4da9-9da0-8d5241ec0fa3"
+      USER_POOL_ID: "us-east-1_Lty7wOM1l",
+      APP_CLIENT_ID: "422mpcglifcu0n7vpn1i31emjt",
+      IDENTITY_POOL_ID: "us-east-1:f55fa677-53fb-4ab2-9c44-99d2ff9afb91"
     }
 };
